ecb5f0d3250d5032e6ff4a2b48b1e0547faf45b8
Joan Corporan <joan.corporan.h@gmail.com>
2023-12-15 21:19:33 -0300

    Actualizando estilos del componente Loading9

--- src/pages/QuienesSP.jsx
+++ src/pages/QuienesSP.jsx
@@ -5,7 +5,12 @@ export const QuienesSP = () => {
   return (
     <div style={{
       width:'100%',
-      height:'50vh'
+      height:'50vh',
+      display:'flex',
+      flexDirection:'row',
+      justifyContent:'center',
+      alignItems:'center'
+
     }}>
       <Loading/>
     </div>
